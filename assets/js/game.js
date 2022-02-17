818f776dc5c815043aad399e3995b03aeb3ed28c
Juan Romo <jmromo1023@gmail.com>
2022-02-16 18:19:51 -0600

    initial development commit

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+//window.alert('This is an alert that javascript is working!');
+
+// getting user input 
+const playerName = window.prompt("what is your robots name?");
+
+//showing user input
+console.log(playerName + "is your robot's name!");
+
+
+//Making our first fight function
+function fight() {
+    window.alert('The Match Has Begun!');
+}
+
+//calling the fight function
+fight();
